6b2ddc9bfa7e880925e86fa252ca200cc8ccfd44
King David <103971670+kyle-david1@users.noreply.github.com>
2022-06-30 19:32:29 -0500

    reorganized Javascript file with all prompts firing correctly

--- README2.md
+++ README2.md
@@ -1,1 +1,9 @@
-code snippet taken from stack overflow to define the arrays
+Title: Random Password Generator
+This project uses javascript components to generate a random password for the user. 
+The user is asked a series of prompts for different criteria to be included in the randomly generated password. 
+After the user answers the prompts satisfactorily, javascript will return a password given the parameters.
+
+This application can be used directly in the browser window. The javascript and CSS files are linked to the HTML, 
+which the user will select to open in default browser. Upon clicking "Generate Password," prompts will display 
+and the user will enter information. 
+
--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,68 @@
-// function userPreferences() {
-//   var pwdLength = parseInt(prompt("how many characters?"))
-//   console.log(pwdLength);
-//   }
-//  if (pwdLength >8 && pwdLength <128){
-//     alert("must be at least 8 characters");
-//  }
- 
+var numberChars = "0123456789";
+var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerChars = "abcdefghijklmnopqrstuvwxyz";
+var specialChars = "/[!@#$%^&*()_+\-=\[\]{};"
+var allChars = [];
 
 
-
-  
-  // var userOptions = {
-  //   wantsUpper: uppercase
-  // }
-  // console.log(userOptions)
-  // return userOptions;
-
-
-
-
-
-
-
-
-
-
-
-
-
-// confirm - lowercase confirm, uppercase, numberic, special characters
-    // let numbers = confirm("do you want numbers?)
-    // console.log(numbers);
-  // create var to hold answer to prompt/confirm 
-
+  var confirmLength = function(item) {
+    if (item<8){
+      passwordLength = window.prompt("Must be greater than 8")
+      confirmLength(passwordLength);
+      // do for all parameters 
+    }
+  }
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
-  let lowercase = window.confirm("do you want lowercase?");
-    console.log(lowercase);
-  let uppercase = window.confirm("do you want uppercase?");
-    console.log(uppercase);
-  let numbers = window.confirm("do you want numbers?");
-    console.log(numbers);
-  let specialCharacters = window.confirm("do you want special characters?");
-    console.log(specialCharacters);
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
-//  This piece of code was firing before adding in the characters list
+
+function generatePassword(){
+let password = ""
+let passwordLength = window.prompt("Password must be between 8 and 128 Characters")
+confirmLength(passwordLength);
+let lowercase = window.confirm("do you want lowercase?");
+  console.log(lowercase);
+  if (lowerChars){
+    allChars.concat(lowerChars);
+  }
+let uppercase = window.confirm("do you want uppercase?");
+  console.log(uppercase);
+  if (upperChars){
+    allChars.concat(upperChars);
+  }
+let numbers = window.confirm("do you want numbers?");
+  console.log(numbers);
+  if (numberChars){
+    allChars.concat(numberChars);
+  }
+let specialCharacters = window.confirm("do you want special characters?");
+  console.log(specialCharacters);
+  if (specialChars){
+    allChars.concat(specialChars);
+  }
+
+  // do for loop here add random letters to password string
+  // how to take a random value from an array 
+  // this index from the array 
+  return password
+}
 
 
-// writePassword(passwordLength)
+
+
+
+
+
+
   
   // list of numbers, special characters, uppercase and lowercase
     // array 
@@ -61,19 +71,6 @@ function writePassword() {
     // create a string set equal to password var
     // array method to check if something is in an array (array.length?)
   
-  
-  var numberChars = "0123456789";
-  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-  var specialChars = "/[!@#$%^&*()_+\-=\[\]{};"
-  var allChars = numberChars + upperChars + lowerChars + specialChars;
-  var randPasswordArray = Array(passwordLength);
-  randPasswordArray[0] = numberChars;
-  randPasswordArray[1] = upperChars;
-  randPasswordArray[2] = lowerChars;
-  randPasswordArray[3] = specialChars;
-  randPasswordArray = randPasswordArray.fill(allChars, 4);
-  return shuffleArray(randPasswordArray.map(function(x) { return x[Math.floor(Math.random() * x.length)] })).join('');
 
 
   // create a string set equal to password var
@@ -90,15 +87,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// function generatePassword(){
-  // your Code goes here 
-
-  // Pseudocode
 
 
-  prompt  = length(8 < 128) 
-    let pwdLength = prompt("how many characters?");
-    console.log(pwdlength)
+
+
 
       // console.log(pwdLength);
   
